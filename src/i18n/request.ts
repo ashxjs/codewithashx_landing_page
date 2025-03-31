b7dea35a949fb6f0c550d715872635533184e694
ashxjs <hurunghee.alexis@gmail.com>
2025-03-31 22:33:49 +0200

    UPDATE: automatic detection

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,12 +1,24 @@
+import { headers } from "next/headers";
 import { getRequestConfig } from "next-intl/server";
 
 export default getRequestConfig(async () => {
   // Provide a static locale, fetch a user setting,
   // read from `cookies()`, `headers()`, etc.
-  const locale = "fr";
+  const headersList = await headers();
+  let locale = headersList.get("accept-language")?.split(",")[0];
+
+  if (!locale) {
+    locale = "fr";
+  }
+
+  if (locale.includes("-")) {
+    locale = locale.split("-")[0];
+  }
+
+  const filePath = await import(`../../locales/${locale}.json`);
 
   return {
     locale,
-    messages: (await import(`../../translation/${locale}.json`)).default,
+    messages: filePath.default,
   };
 });
